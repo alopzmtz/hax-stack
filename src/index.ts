e88235ebdf88a9956f1f6e848e90813c75709763
Alejandro Lopez <alejandro@alopezmtz.com>
2024-11-17 12:13:22 -0600

    added vite as build tools

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import { serve } from '@hono/node-server'
 import 'dotenv/config'
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
@@ -11,13 +10,6 @@ const app = new Hono()
 app.use(cors())
 app.use(logger())
 
-app.use('/public/*', serveStatic({ root: './src/' }))
-app.use('/favicon.ico', serveStatic({ path: './src/public/favicon.ico'}))
-
 app.route('/', routes)
 
-
-const port = 8787 
-console.log(`Server is running on http://localhost:${port}`)
-
-serve({ fetch: app.fetch, port })
+export default app
