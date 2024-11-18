6e539766b738dacd7b4d13d0db53e98941e6c7d9
Alejandro Lopez <alejandro@alopezmtz.com>
2024-11-18 08:19:52 -0600

    refactor: remove Vite configuration, update TypeScript settings, and modify start script for improved server setup

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
+import 'dotenv/config'
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
-import 'vite/modulepreload-polyfill'
 import { logger } from './middleware/logger.js'
 import routes from './routes/index.js'
 import { serveStatic } from '@hono/node-server/serve-static'
@@ -11,10 +11,9 @@ const app = new Hono()
 app.use(cors())
 app.use(logger())
 
+app.use('*', serveStatic({ root: './public' }))
+app.use('/favicon.ico', serveStatic({ path: './public/favicon.ico' }))
+
 app.route('/', routes)
 
-if (import.meta.env.PROD) {
-    serve({ fetch: app.fetch, port: 8080 }, () => console.log('Server is running on http://localhost:8080'))
-}
-
-export default app
+serve({ fetch: app.fetch, port: 8080 }, () => console.log('Server is running on http://localhost:8080'))
