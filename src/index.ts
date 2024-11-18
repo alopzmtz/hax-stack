d5a73a50992e16d967c61a16fca245459aa868a7
Alejandro Lopez <alejandro@alopezmtz.com>
2024-11-17 23:24:16 -0600

    refactor: update build configuration and server setup

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,16 @@ import { cors } from 'hono/cors'
 import { serveStatic } from '@hono/node-server/serve-static'
 import routes from './routes/index.js'
 import { logger } from './middleware/logger.js'
+import { serve } from '@hono/node-server'
 
 const app = new Hono()
 
 app.use(cors())
 app.use(logger())
 
+app.use('/public/*', serveStatic({ root: './' }))
+app.use('/favicon.ico', serveStatic({ path: './public/favicon.ico' }))
+
 app.route('/', routes)
 
-export default app
+serve({ fetch: app.fetch, port: 8080 }, () => { console.log('Server is running on http://localhost:8080') })
